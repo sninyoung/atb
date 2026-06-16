5ccccac6587ca9b0de923524e937fdbc6cf9fd2f
sninyy <128959123+sninyoung@users.noreply.github.com>
2026-06-17 01:01:22 +0900

    수정
    사업분야 내용 수정
    메인페이지 글씨 수정
    이미지 배치 수정, 이미지 추가&삭제
    메인 페이지 비주얼 스위퍼 넘어가는 시간 축소

--- js/main.js
+++ js/main.js
@@ -4,18 +4,14 @@
 */
 
 $(document).ready(function(){
-    
-    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+    const visual_swiper = new Swiper('.visual .swiper', { 
 
         autoplay: {  /* 팝업 자동 실행 */
-            delay: 5000,
+            delay: 3000, /* main.css에서 .visual .ctrl_wrap .paging span.swiper-pagination-current::after의 animation-duration 값을 바꿔줌. 3000이면 3s로 */
             disableOnInteraction: true,
         },
-
         effect: "fade", /* fade 효과 */
-
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-
         pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
             el: '.visual .ctrl_wrap .paging', /* 해당 요소의 class명 */
             clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
@@ -24,20 +20,7 @@ $(document).ready(function(){
                 return '<span class="' + className + '">' + (index + 1) + "</span>";
             },
         },
-
     });
-
-    $('.visual .ctrl_wrap button.stop').on('click', function(){
-        visual_swiper.autoplay.stop();  /* 일시정지 기능 */
-        $(this).hide()
-        $('.visual .ctrl_wrap button.play').show()
-    })
-    $('.visual .ctrl_wrap button.play').on('click', function(){
-        visual_swiper.autoplay.start();  /* 재생 기능 */
-        $(this).hide()
-        $('.visual .ctrl_wrap button.stop').show()
-    })
-
 
     /* biz에 마우스를 오버했을 때 (시작)
     *
